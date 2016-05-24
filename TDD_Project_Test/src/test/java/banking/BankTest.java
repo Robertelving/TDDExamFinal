9e97686c1609229650bc746cee3e6b90e1ec3d3b
robertelving <vivalaklask@gmail.com>
2016-05-24 17:55:16 +0200

    Added TODO LIST
    Author: Robert

--- TDD_Project_Test/src/test/java/banking/BankTest.java
+++ TDD_Project_Test/src/test/java/banking/BankTest.java
@@ -5,10 +5,18 @@ import org.junit.Before;
 import org.junit.Test;
 import static org.mockito.Mockito.*;
 
-/**
- *
- * @author Skroget
- */
+//TODO
+/*
+    Withdraw less than 20000 on any account with sufficient funds 	| Expect Success
+    Withdraw more that 20000 on any account 			  	| Expect Exception
+    Withdraw from CREDIT account so final balance > -30000 		| Expect Success
+    Withdraw from CREDIT account so final balance < -30000		| Expect Exception
+    Withdraw from DEBIT account so final balance >= 0			| Expect Success
+    Withdraw from DEBIT account so final balance < -			| Expect Exception
+    Deposit to FUND account 5000					| Expect Success
+    Deposit to FUND account 5001					| Expect Exception
+    
+*/
 public class BankTest {
 
     private BankIF bank = null;
