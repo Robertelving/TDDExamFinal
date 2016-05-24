8fe3a522d3c96495ba580860c8f73e55e3c77366
robertelving <vivalaklask@gmail.com>
2016-05-24 17:49:13 +0200

    Test clean
    Author: Robert

--- TDD_Project_Test/src/test/java/banking/BankTest.java
+++ TDD_Project_Test/src/test/java/banking/BankTest.java
@@ -5,37 +5,34 @@ import org.junit.Before;
 import org.junit.Test;
 import static org.mockito.Mockito.*;
 
-
 /**
  *
  * @author Skroget
  */
 public class BankTest {
 
-
-    @Before
-    public void setUp() {        
-
-    }
-
-    @Test(expected=NegativeTransferAmountException.class)
-    public void transferMethodTestNegtiveTransferAmount() throws Exception {
-
-        BankIF bank = null;
-        AccountIF acc1 = null, acc2 = null;
+    private BankIF bank = null;
+    private AccountIF acc1 = null, acc2 = null;
+    private BigDecimal transferAmount;
     
+    @Before
+    public void setUp() {
         //Actors
         bank = new Bank();
         acc1 = mock(AccountIF.class);
-        acc2 = mock(AccountIF.class); 
-        
-        final BigDecimal transferAmount = BigDecimal.valueOf(-500);
-        
+        acc2 = mock(AccountIF.class);
+    }
+
+    @Test(expected = NegativeTransferAmountException.class)
+    public void transferMethodTestNegtiveTransferAmount() throws Exception {
+
+        transferAmount = BigDecimal.valueOf(-500);
+
         //execute
         bank.transfer(acc1, acc2, transferAmount);
-        verify(acc1,never()).withdraw(transferAmount);
-        verify(acc2,never()).deposit(transferAmount);
-        
+        verify(acc1, never()).withdraw(transferAmount);
+        verify(acc2, never()).deposit(transferAmount);
+
     }
-    
+
 }
