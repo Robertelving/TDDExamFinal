d4bb00475debaf3c83f35e3048c850eed374d7ec
robertelving <vivalaklask@gmail.com>
2016-05-24 19:16:01 +0200

    Added UnrecAccountType test
    Author: Robert

--- TDD_Project_Test/src/test/java/banking/BankTest.java
+++ TDD_Project_Test/src/test/java/banking/BankTest.java
@@ -7,22 +7,22 @@ import static org.mockito.Mockito.*;
 
 //TODO
 /*
-    Withdraw less than 20000 on any account with sufficient funds 	| Expect Success
-    Withdraw more that 20000 on any account 			  	| Expect Exception
-    Withdraw from CREDIT account so final balance > -30000 		| Expect Success
-    Withdraw from CREDIT account so final balance < -30000		| Expect Exception
-    Withdraw from DEBIT account so final balance >= 0			| Expect Success
-    Withdraw from DEBIT account so final balance < -			| Expect Exception
-    Deposit to FUND account 5000					| Expect Success
-    Deposit to FUND account 5001					| Expect Exception
+ Withdraw less than 20000 on any account with sufficient funds 	| Expect Success
+ Withdraw from CREDIT account so final balance > -30000 		| Expect Success
+ Withdraw from CREDIT account so final balance < -30000		| Expect Exception
+ Withdraw from DEBIT account so final balance >= 0			| Expect Success
+ Withdraw from DEBIT account so final balance < -			| Expect Exception
+ Deposit to FUND account 5000					| Expect Success
+ Deposit to FUND account 5001					| Expect Exception
+ Account Type Not Recognized                                         | Expect Exception
     
-*/
+ */
 public class BankTest {
 
     private BankIF bank = null;
     private AccountIF acc1 = null, acc2 = null;
     private BigDecimal transferAmount;
-    
+
     @Before
     public void setUp() {
         //Actors
@@ -42,16 +42,30 @@ public class BankTest {
         verify(acc2, never()).deposit(transferAmount);
 
     }
-    
+
     @Test(expected = MoneyLaunderingException.class)
     public void transferMethodTestOverUpperBoundaryTestAmount() throws Exception {
-        
+
         transferAmount = BigDecimal.valueOf(20001);
-       
+
         //execute
         bank.transfer(acc1, acc2, transferAmount);
         verify(acc1, never()).withdraw(transferAmount);
         verify(acc2, never()).deposit(transferAmount);
+    }
+
+    @Test(expected = UnrecognizedAccountTypeException.class)
+    public void transferMethodTestUnrecognizedException() throws Exception {
+
+        transferAmount = BigDecimal.valueOf(500);
+
+        when(acc1.getAccountType()).thenReturn("UnrecognizedType");
+        
+        //execute
+        bank.transfer(acc1, acc2, transferAmount);
+        verify(acc1, never()).withdraw(transferAmount);
+        verify(acc2, never()).deposit(transferAmount);
+
     }
 
 }
