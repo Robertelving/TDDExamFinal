99e004c0c446c3f94c64103038bbe66a71707403
robertelving <vivalaklask@gmail.com>
2016-05-24 18:35:23 +0200

    Added UpperBoundaryTest Implementation
    Author: Robert

--- TDD_Project_Test/src/main/java/banking/Bank.java
+++ TDD_Project_Test/src/main/java/banking/Bank.java
@@ -9,11 +9,13 @@ import java.math.BigDecimal;
 public class Bank implements BankIF {
 
     @Override
-    public void transfer(AccountIF sender, AccountIF receiver, BigDecimal amount) throws NegativeTransferAmountException {
-
+    public void transfer(AccountIF sender, AccountIF receiver, BigDecimal amount) throws NegativeTransferAmountException, MoneyLaunderingException {
+        
         if (amount.compareTo(BigDecimal.ZERO) == -1) {
             throw new NegativeTransferAmountException("negative transfer amount");
-        }
+        }else if(amount.compareTo(BigDecimal.valueOf(20000)) == 1){
+            throw new MoneyLaunderingException("Drug Money Detected");
+        } 
 
     }
 
